a77e7f0caaf9ec8ca454a58045d8a79793d4cd33
Paweł Gawlik <77546453+PawelGawlik@users.noreply.github.com>
2022-04-24 22:39:47 +0200

    change config.js

--- config.js
+++ config.js
@@ -1,5 +1,4 @@
 module.exports = {
     keySession: ['sesja'],
-    maxAgeSession: 3600 * 1000,
-    db: "mongodb+srv://Pawgaw:CcaD5900@cluster0.dx8wf.mongodb.net/sf?retryWrites=true&w=majority"
+    maxAgeSession: 3600 * 1000
 }
