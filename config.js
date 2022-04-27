55f02ed4b2ac495aa5d2df84c27b4406fe5e5359
PawelGawlik <mojaulub.6@gmail.com>
2022-04-27 13:08:19 +0200

    change config.js

--- config.js
+++ config.js
@@ -1,5 +1,6 @@
 module.exports = {
     keySession: ['sesja'],
     maxAgeSession: 3600 * 1000,
-    db: "mongodb+srv://Pawgaw:CcaD5900@cluster0.dx8wf.mongodb.net/sf?retryWrites=true&w=majority"
+    db: process.env.MONGODB_URI
+    // db: "mongodb+srv://Pawgaw:CcaD5900@cluster0.dx8wf.mongodb.net/sf?retryWrites=true&w=majority"
 }
