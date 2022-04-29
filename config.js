60f808b78fde16f1144b46375ae65c297b7d8d4c
PawelGawlik <mojaulub.6@gmail.com>
2022-04-29 21:13:58 +0200

    update config.js and remove nodemon.json

--- config.js
+++ config.js
@@ -2,5 +2,4 @@ module.exports = {
     keySession: ['sesja'],
     maxAgeSession: 3600 * 1000,
     db: process.env.MONGODB_URI
-    // db: "mongodb+srv://Pawgaw:CcaD5900@cluster0.dx8wf.mongodb.net/sf?retryWrites=true&w=majority"
 }
