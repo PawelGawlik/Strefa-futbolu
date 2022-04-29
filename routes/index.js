a63c3f37abc428fa95a5d9efe237d5aa4c2e024f
PawelGawlik <mojaulub.6@gmail.com>
2022-04-29 16:28:00 +0200

    update index

--- routes/index.js
+++ routes/index.js
@@ -273,7 +273,7 @@ const funkcja1 = (par1) => {
         //})
     })
     par1.get('/news.html/:wiad?', (req, res) => {
-        res.sendFile('news.html', { root: '../public' });
+        res.sendFile('news.html', { root: 'C:/users/gawy/onedrive/dokumenty/sf/public' });
     })
     par1.post('/news.html/:wiad', (req, res) => {
         if (user === "") {
