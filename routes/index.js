f8bf3e8c7fbecaa6a8fd749dd7baa3fb2ae2dbd0
PawelGawlik <mojaulub.6@gmail.com>
2022-04-29 13:21:44 +0200

    update index

--- routes/index.js
+++ routes/index.js
@@ -6,7 +6,7 @@ const funkcja1 = async (par1) => {
     const db = client.db('sf');
     const users = db.collection('users');
     const main = db.collection('main');
-    await client.connect();
+    //await client.connect();
     //users.find().toArray((err, data) => {
     // console.log(data);
     //})
